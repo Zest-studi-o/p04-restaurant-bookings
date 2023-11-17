93b5db5adc395207a2184047637b7085c9980fae
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-11-17 23:53:25 +0000

    Fix indentation error

--- megarestaurant/settings.py
+++ megarestaurant/settings.py
@@ -110,12 +110,12 @@ WSGI_APPLICATION = 'megarestaurant.wsgi.application'
      #'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
          #'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+       #}
+  # }
 
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+}
 
 
 # Password validation
