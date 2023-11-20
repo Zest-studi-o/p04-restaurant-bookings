6650df836b5cd3323752e9f1e4050fd7eceea693
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-11-20 16:11:50 +0000

    Fix most python linter errors at models.py

--- bookatable/models.py
+++ bookatable/models.py
@@ -3,14 +3,20 @@ from django.contrib.auth.models import User
 from django.db.models import Sum
 
 
-PEOPLE = ((1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "4"), (6, "6"), (7, "7"), (8, "8"), (9, "9"), (10, "10"), (11, "11"), (12, "12"))
+PEOPLE = (
+            (1, "1"), (2, "2"), (3, "3"),
+            (4, "4"), (5, "4"), (6, "6"),
+            (7, "7"), (8, "8"), (9, "9"),
+            (10, "10"), (11, "11"), (12, "12")
+        )
+
 
 class Booking(models.Model):
-    id = models.AutoField(primary_key=True) # Set this to primary key
+    id = models.AutoField(primary_key=True)  # Set this to primary key
     date = models.DateField()
     time = models.CharField(
         max_length=20,
-        choices=[ 
+        choices=[
             ("10:00", "10:00 - 10:45"),
             ("11:00", "11:00 - 11:45"),
             ("12:00", "12:00 - 12:45"),
@@ -25,13 +31,16 @@ class Booking(models.Model):
             ("21:00", "21:00 - 21:45"),
             ("22:00", "22:00 - 22:45"),
         ],
-         default="10:00 - 10:45"  # Default shown is "10:00- 10:45"
+        default="10:00 - 10:45"  # Default shown is "10:00- 10:45"
     )
-    name = models.CharField(max_length=255) # Name 
-    email = models.EmailField() # Email
-    people = models.IntegerField(choices=PEOPLE, default=2) # To select how many people 
-    created_on = models.DateTimeField(auto_now_add=True) # Registers the data when it was created
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE) # Set this to foreign key
+    name = models.CharField(max_length=255)  # Name
+    email = models.EmailField()  # Email
+    people = models.IntegerField(choices=PEOPLE, default=2)
+    # To select how many people
+    created_on = models.DateTimeField(auto_now_add=True)
+    # Registers the data when it was created
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Set this to foreign key
 
     def is_available(self):
         bookings = Booking.objects.filter(date=self.date, time=self.time).aggregate(Sum('people'))['people__sum'] or 0
@@ -41,8 +50,8 @@ class Booking(models.Model):
         return bookings < 12
 
     def save(self, *args, **kwargs):
-        """ 
-        Check availability 
+        """
+        Check availability
         """
         if not self.id:
             if not self.is_available():
